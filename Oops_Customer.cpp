1cccd617f1d49480c44ed8d5ae19c4ff002b0090
Kartik Ambhore <kartikambhore26@gmail.com>
2026-08-16 20:47:56 +0530

    Add sample output for Customer Management System functionality

--- Oops_Customer.cpp
+++ Oops_Customer.cpp
@@ -143,4 +143,171 @@ int main() {
     }while(choice != 5);
     return 0;
 }
-//have done some modification in code after previous completion.
+
+/*
+
+SAMPLE OUTPUT
+
+Customer Management System
+------------------------
+1. Insert Customer Info
+2. Update Customer Info
+3. Delete Customer Info
+4. Display Customer Info
+5. Exit
+
+Enter your choice: 1
+
+Enter the number of customers to insert: 2
+Enter Customer Name: kartik ambhore
+Enter Customer Age: 19
+Enter Customer Email: kartik@gmail.com
+Enter Bought Items: bread
+Enter Price: 54
+
+Enter Customer Name: soham more
+Enter Customer Age: 17
+Enter Customer Email: soham@gmai;.com
+Enter Bought Items: toast
+Enter Price: 12
+
+
+Customer Management System
+------------------------
+1. Insert Customer Info
+2. Update Customer Info
+3. Delete Customer Info
+4. Display Customer Info
+5. Exit
+
+Enter your choice: 1
+
+Enter the number of customers to insert: 1
+Enter Customer Name: sahil more
+Enter Customer Age: 17 
+Enter Customer Email: sahil@gmail.com
+Enter Bought Items: jam
+Enter Price: 12
+
+
+Customer Management System
+------------------------
+1. Insert Customer Info
+2. Update Customer Info
+3. Delete Customer Info
+4. Display Customer Info
+5. Exit
+
+Enter your choice: 2
+
+Enter the index of the customer to update: 1
+Enter Updated Customer Name: soham nirne
+Enter Updated Customer Age: 19
+Enter Updated Customer Email: sohamnirne@86
+Enter Updated Bought Items: ketcup
+Enter Updated Price: 43
+Customer info updated.
+
+Customer Management System
+------------------------
+1. Insert Customer Info
+2. Update Customer Info
+3. Delete Customer Info
+4. Display Customer Info
+5. Exit
+
+Enter your choice: 4
+
+Customer Information:
+------------------------
+Customer 1:
+Name: kartik ambhore
+Age: 19
+Email: kartik@gmail.com
+Bought Items: read
+Price: 54
+------------------------
+
+Customer 2:
+Name: soham nirne
+Age: 19
+Email: sohamnirne@86
+Bought Items: etcup
+Price: 43
+------------------------
+
+Customer 3:
+Name: sahil more
+Age: 17
+Email: sahil@gmail.com
+Bought Items: am
+Price: 12
+------------------------
+
+
+Customer Management System
+------------------------
+1. Insert Customer Info
+2. Update Customer Info
+3. Delete Customer Info
+4. Display Customer Info
+5. Exit
+
+Enter your choice: 6
+
+exiting the program.
+
+Customer Management System
+------------------------
+1. Insert Customer Info
+2. Update Customer Info
+3. Delete Customer Info
+4. Display Customer Info
+5. Exit
+
+Enter your choice: 3 
+
+Enter the index of the customer to delete: 2
+Customer info deleted.
+
+Customer Management System
+------------------------
+1. Insert Customer Info
+2. Update Customer Info
+3. Delete Customer Info
+4. Display Customer Info
+5. Exit
+
+Enter your choice: 4
+
+Customer Information:
+------------------------
+Customer 1:
+Name: kartik ambhore
+Age: 19
+Email: kartik@gmail.com
+Bought Items: read
+Price: 54
+------------------------
+
+Customer 2:
+Name: soham nirne
+Age: 19
+Email: sohamnirne@86
+Bought Items: etcup
+Price: 43
+------------------------
+
+
+Customer Management System
+------------------------
+1. Insert Customer Info
+2. Update Customer Info
+3. Delete Customer Info
+4. Display Customer Info
+5. Exit
+
+Enter your choice: 5
+
+exiting the program.
+*/
