99e25d18eaff1a832c86122fb4dc08c8555cc67a
Kartik Ambhore <kartikambhore26@gmail.com>
2026-09-22 00:19:06 +0530

    Implement stack class with push, pop, isEmpty, and priority methods

--- stack.cpp
+++ stack.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 using namespace std;
 class stack{
-    int data;
+    int top;
+    char arr[50];
+    public:
+        stack(){
+            top=-1;
+        }
+        void push(int ch){
+            arr[++top]=ch;
+        }
+        char pop(){
+            return arr[top--];
+        }
+        bool isEmpty(){
+            return (top==-1);
+        }
+        int priority(char ch){
+            if (ch=='^') return 3;
+            if (ch=='*' || ch=='/') return 2;
+            if (ch=='+' || ch=='-') return 1;
+            return 0;
+        }
 };
 int main() {
     
